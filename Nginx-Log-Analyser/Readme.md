ba562d1bfa51f762b640e81851c6c47df6bda7f5
linuxlover6509 <badshodam11@gmail.com>
2026-05-28 18:07:19 +0300

    lab-03

---
+++ Nginx-Log-Analyser/Readme.md
@@ -0,0 +1,14 @@
+## Server Performance Stats
+
+# Usage
+
+Make the script executable
+   `chmod +x ./nginxLogAnalyser.sh`
+
+Run
+```
+./nginxLogAnalyser.sh /path/to/logFile.log
+```
+
+This project is part of [roadmap.sh](https://roadmap.sh/projects/server-stats) DevOps projects.
+
---
+++ Nginx-Log-Analyser/nginxLogAnalyser.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+LOGFILE=$1
+
+
+[[ $LOGFILE ]] || { echo "No file or no argument.";exit 1; }
+
+
+
+echo "Top 5 IP addresses with the most requests:"
+awk '{print $1}' $LOGFILE | uniq -c | sort -nr | head -n5
+echo -e "\nTop 5 most requested paths:"
+awk '{print $7}' $LOGFILE | sort | uniq -c | sort -nr | head -n5
+echo -e "\nTop 5 response status codes:"
+awk '{print $9}' $LOGFILE | sort | uniq -c | sort -nr | head -n5
+echo -e "\nTop 5 user agents:"
+awk -F'"' '{print $6}' $LOGFILE | sort | uniq -c | sort -nr | head -n5
